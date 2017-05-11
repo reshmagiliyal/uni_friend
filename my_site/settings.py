8d0eee6564ee27746e89b7af2bf7004c8ece7dc0
reshmagiliyal <reshma.giliyal@gmail.com>
2017-05-11 15:03:42 -0700

    adding settings un comment static_root

--- my_site/settings.py
+++ my_site/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-#     # '/var/www/static/'
-# )
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+    '/var/www/static/'
+)
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
