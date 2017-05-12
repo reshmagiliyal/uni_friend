587cc41f7d3527836e270867043b83b53c49082e
reshmagiliyal <reshma.giliyal@gmail.com>
2017-05-11 17:35:36 -0700

    adding gunicorn in settings

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "my_site.settings")
     try:
         from django.core.management import execute_from_command_line
